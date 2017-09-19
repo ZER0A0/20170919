91a40c07d0a00462bc366421af5ae84a89fd0a65
ZER0A0 <105021041@live.asia.edu.tw>
2017-09-19 12:44:26 +0800

    First

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,13 +1,32 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame{
+    private Button btnExit = new Button("Exit");
+    private Button btnAdd = new Button("Add");
     public MainFrame() {
         initComp();
     }
     private void initComp(){
         this.setBounds(100,120,400,300);
+        this.setLayout(null);
+        btnExit.setBounds(80,80,80,80);
+        btnAdd.setBounds(80,160,80,80);
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }});
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }});
+        this.add(btnExit);
+        this.add(btnAdd);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
